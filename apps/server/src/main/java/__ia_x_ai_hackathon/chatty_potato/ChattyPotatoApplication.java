4ab62eb852d6647a8ea1919fecd8829037b3d0a5
Heesung Choi <gladhee@icloud.com>
2025-11-01 16:39:40 +0900

    initialize project structure with Gradle and Spring Boot setup

---
+++ apps/server/src/main/java/__ia_x_ai_hackathon/chatty_potato/ChattyPotatoApplication.java
@@ -0,0 +1,13 @@
+package __ia_x_ai_hackathon.chatty_potato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattyPotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattyPotatoApplication.class, args);
+	}
+
+}
