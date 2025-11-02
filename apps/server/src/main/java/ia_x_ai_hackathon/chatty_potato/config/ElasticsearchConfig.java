ce5df751c7e0883a748a1c96df8fcb00ccbb2185
Heesung Choi <gladhee@icloud.com>
2025-11-02 09:51:28 +0900

    feat: update BedrockEmbeddingConfig and ChatController for improved functionality and versioning

--- apps/server/src/main/java/ia_x_ai_hackathon/chatty_potato/config/ElasticsearchConfig.java
+++ apps/server/src/main/java/ia_x_ai_hackathon/chatty_potato/config/ElasticsearchConfig.java
@@ -5,14 +5,14 @@ import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@Configuration
-@EnableElasticsearchRepositories(basePackages = "ia_x_ai_hackathon.chatty_potato.rag.repository")
-public class ElasticsearchConfig extends ElasticsearchConfiguration {
-
-    @Override
-    public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo("localhost:9200") // Elasticsearch 주소
-                .build();
-    }
-}
+//@Configuration
+//@EnableElasticsearchRepositories(basePackages = "ia_x_ai_hackathon.chatty_potato")
+//public class ElasticsearchConfig extends ElasticsearchConfiguration {
+//
+//    @Override
+//    public ClientConfiguration clientConfiguration() {
+//        return ClientConfiguration.builder()
+//                .connectedTo("localhost:9200") // Elasticsearch 주소
+//                .build();
+//    }
+//}
